d328d569cee3315bf7dd4b4c7b1a52663d896e74
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-04 23:34:24 +0200

    Use an empty Android Studio project as base

---
+++ app/src/main/java/name/juhasz/judit/audiobooks/UserBooksActivity.java
@@ -0,0 +1,13 @@
+package name.juhasz.judit.audiobooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_books);
+    }
+}
