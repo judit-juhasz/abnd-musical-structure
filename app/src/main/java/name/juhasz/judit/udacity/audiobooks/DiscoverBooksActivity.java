799b8832fca558b4861c6c7e72998606f7d230f0
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-05 21:50:43 +0200

    Prepare for launching DiscoverBooksActivity

---
+++ app/src/main/java/name/juhasz/judit/udacity/audiobooks/DiscoverBooksActivity.java
@@ -0,0 +1,13 @@
+package name.juhasz.judit.udacity.audiobooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiscoverBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover_books);
+    }
+}
