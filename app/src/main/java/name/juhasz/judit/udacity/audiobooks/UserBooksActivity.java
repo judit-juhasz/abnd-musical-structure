78f5bc27cccc987db19f08033e2f950dc22b9856
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-05 22:04:25 +0200

    Allow the user to open DiscoverBooksActivity

--- app/src/main/java/name/juhasz/judit/udacity/audiobooks/UserBooksActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/audiobooks/UserBooksActivity.java
@@ -1,7 +1,10 @@
 package name.juhasz.judit.udacity.audiobooks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserBooksActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class UserBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_books);
+
+        Button discoverButton = (Button) findViewById(R.id.b_discover);
+
+        discoverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent discoverIntent = new Intent(UserBooksActivity.this, DiscoverBooksActivity.class);
+                startActivity(discoverIntent);
+            }
+        });
     }
 }
